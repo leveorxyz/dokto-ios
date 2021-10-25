e41a16c805178dc6aa2a9f64aafb4cafc1c10127
Sanviraj Zahin Haque <zahinjason220434@gmail.com>
2021-10-25 12:23:51 +0600

    Add basic Twilio Sdk variables

--- Dokto/Modules/Dashboard/Controllers/TwilioRoomViewController.swift
+++ Dokto/Modules/Dashboard/Controllers/TwilioRoomViewController.swift
@@ -6,13 +6,25 @@
 //
 
 import UIKit
+import TwilioVideo
 
 class TwilioRoomViewController: UIViewController {
+    
+    var room : Room?
+    var audioTrack : LocalAudioTrack?
+    var videoTrack : LocalVideoTrack?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
 
