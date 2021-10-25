81390225a6726d9197ae84586a715b1e89c52b3f
Sanviraj Zahin Haque <zahinjason220434@gmail.com>
2021-10-25 15:33:36 +0600

    Get token from Backend server

---
+++ Dokto/Managers/TwilioManager.swift
@@ -0,0 +1,13 @@
+//
+//  TwilioManager.swift
+//  Dokto
+//
+//  Created by Sanviraj Zahin Haque on 25/10/21.
+//
+
+import Foundation
+
+
+class TwilioManager{
+    
+}
--- Dokto/Modules/Dashboard/Controllers/TwilioRoomViewController.swift
+++ Dokto/Modules/Dashboard/Controllers/TwilioRoomViewController.swift
@@ -13,7 +13,10 @@ class TwilioRoomViewController: UIViewController {
     var room : Room?
     var audioTrack : LocalAudioTrack?
     var videoTrack : LocalVideoTrack?
-
+    
+    @IBOutlet weak var UserNameField: UITextField!
+    @IBOutlet weak var RoomNameField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,5 +30,53 @@ class TwilioRoomViewController: UIViewController {
         self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
-
+    
+    @IBAction func joinRoomAction(_ sender: Any) {
+        guard let userName = UserNameField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ,
+              let roomName = RoomNameField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+              !userName.isEmpty, !roomName.isEmpty else{
+                  print("Enter a valid userName and room name")
+                  return
+              }
+        let urlString = "http://159.203.72.156/twilio/video-token/"
+        
+        guard let url = URL(string: urlString) else{
+            print("bad URL")
+            return
+        }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let data : [String : Any] = [
+            "username" : userName,
+            "room_name" : roomName
+        ]
+        let requestData = try? JSONSerialization.data(withJSONObject: data)
+        urlRequest.httpBody = requestData
+        URLSession.shared.dataTask(with: urlRequest) { data, fetchedResponse , error in
+            guard let data = data, error == nil else{
+                
+                print("No data found")
+                return
+            }
+            //debugPrint(fetchedResponse)
+            do{
+                let tokenResponse = try JSONDecoder().decode(TwilioVideoTokenResponse.self, from: data)
+                debugPrint(tokenResponse)
+            }
+            catch{
+                print("Error here")
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+    
 }
+
+struct TwilioVideoTokenResponseElement : Codable{
+    let status_code : Int
+    let message : String
+    let token : String
+}
+
+typealias TwilioVideoTokenResponse = [TwilioVideoTokenResponseElement]
