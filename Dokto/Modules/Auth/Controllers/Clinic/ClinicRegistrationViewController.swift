73d83d18071bc729f8d64d5df463cd5ded95befa
Sanviraj Zahin Haque <zahinjason220434@gmail.com>
2021-11-29 16:36:45 +0600

    Add: Clinic registration view controller

---
+++ Dokto/Modules/Auth/Controllers/Clinic/ClinicRegistrationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ClinicRegistrationViewController.swift
+//  Dokto
+//
+//  Created by Sanviraj Zahin Haque on 29/11/21.
+//
+
+import UIKit
+
+class ClinicRegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
