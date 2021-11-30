6bdd99d4ea5ba2a1f32802a57c0aef261d7cf52c
Sanviraj Zahin Haque <zahinjason220434@gmail.com>
2021-11-30 12:35:29 +0600

    Add: IBoutlets for fields, Fix: Zip code placeholder

--- Dokto/Modules/Auth/Controllers/Clinic/ClinicRegistrationViewController.swift
+++ Dokto/Modules/Auth/Controllers/Clinic/ClinicRegistrationViewController.swift
@@ -9,6 +9,27 @@ import UIKit
 
 class ClinicRegistrationViewController: UIViewController {
 
+    
+    @IBOutlet weak var clinicImageView: UIImageView!
+    @IBOutlet weak var hospitalNameField: UITextField!
+    @IBOutlet weak var mobileNumberCountryCodeField: UITextField!
+    @IBOutlet weak var mobilenumberField: UITextField!
+    @IBOutlet weak var mobileNumberdropDownButton: UIButton!
+    @IBOutlet weak var clinicEmailField: UITextField!
+    @IBOutlet weak var passWordField: UITextField!
+    @IBOutlet weak var confirmPasswordField: UITextField!
+    @IBOutlet weak var NumberofPractitionersField: UITextField!
+    @IBOutlet weak var countrySelectField: UITextField!
+    @IBOutlet weak var countrySelectButton: UIButton!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var stateSelectField: UITextField!
+    @IBOutlet weak var stateSelectButton: UIButton!
+    @IBOutlet weak var citySelectField: UITextField!
+    @IBOutlet weak var citySelectButton: UIButton!
+    @IBOutlet weak var zipCodeField: UITextField!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
